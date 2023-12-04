3d15389d07608af9a5e059a599eea794b6b8656c
Abderrahmane El Mahmi <elmahmi.a04@gmail.com>
2023-12-04 21:23:39 +0000

    Form lib supports custom constraints
    Form lib has validation hook to check field value before submission.
    Users can also return custom error messages

--- lib/Framework/Form.php
+++ lib/Framework/Form.php
@@ -12,7 +12,15 @@ class Form {
     private $inputs;
     private $formValues;
     private $errors = [];
-
+    private $userConstraints = [];
+    
+    /**
+     * Create a new Form
+     *
+     * @param  string $formView Name of form blade file. Must be in app/views/forms/
+     * @param  Session $session 
+     * @param  array $bladeData Data to pass to form when rendering using blade
+     */
     public function __construct(private string $formView, private Session $session, array $bladeData = []) {
         $bladeRendered = Blade::make("forms.$formView", $bladeData);
         
@@ -28,7 +36,12 @@ class Form {
             $this->formValues = new InputBag();
         }
     }
-    
+        
+    /**
+     * Generates form - adds persistent input values, inserts errors, removes client-side validation
+     *
+     * @return string
+     */
     public function render() : string {
         foreach ($this->inputs as $input) {
             $input->removeAttribute("required");
@@ -50,26 +63,34 @@ class Form {
         
         return $this->dom->saveHTML();
     }
-
+    
+    /**
+     * Validates form input
+     *
+     * @param  InputBag $formValues
+     * @return bool true if form is ok, false if form fails 
+     */
     public function validate(InputBag $formValues) : bool {
         $this->formValues = $formValues;
         $this->errors = [];
         
         $noError = true;
-        $constraints = [
-            "required" => [fn($value) => $value === "", "This field is required"],
-            "minlength" => [fn($value, $attr) => strlen($value) < intval($attr), "Minimum length is %s"],
-            "maxlength" => [fn($value, $attr) => strlen($value) > intval($attr), "Maximum length is %s"],
+        $attributeConstraints = [
+            "required" => [fn($value) => $value !== "", "This field is required"],
+            "minlength" => [fn($value, $attr) => strlen($value) >= intval($attr), "Minimum length is %s"],
+            "maxlength" => [fn($value, $attr) => strlen($value) <= intval($attr), "Maximum length is %s"],
         ];
 
         foreach ($this->inputs as $input) {
-            $noError = $this->validateField($input, "required", ...$constraints["required"]) && $noError;
+            $noError = $this->validateAttribute($input, "required", ...$attributeConstraints["required"]) && $noError;
         
             $inputName = $input->getAttribute("name");
             if ($this->formValues->get($inputName) === "") continue;
             
-            $noError = $this->validateField($input, "minlength", ...$constraints["minlength"]) && $noError;
-            $noError = $this->validateField($input, "maxlength", ...$constraints["maxlength"]) && $noError;
+            $noError = $this->validateAttribute($input, "minlength", ...$attributeConstraints["minlength"]) && $noError;
+            $noError = $this->validateAttribute($input, "maxlength", ...$attributeConstraints["maxlength"]) && $noError;
+
+            $noError = $this->validateConstraints($inputName) && $noError;
         }
         
         $this->session->set("form-" . $this->formView, [
@@ -86,19 +107,62 @@ class Form {
         $this->formValues = new InputBag();
         $this->errors = [];
     }
-
-    private function validateField(HTML5DOMElement $input, string $attribute, \Closure $condition, string $errorMessage) : bool {
+    
+    /**
+     * Add Validation check to form field
+     *
+     * @param  string $inputName Name attribute of Input field
+     * @param  \Closure $condition Anonymous function with condition. 
+     *                             Must take 1 parameter (value of input) and return bool (true if no error, false otherwise)
+     * @param  string $errorMessage Error to be displayed if input fails validation
+     * @return void
+     */
+    public function addConstraint(string $inputName, \Closure $condition, string $errorMessage) {
+        $this->userConstraints[$inputName][] = ["condition" => $condition, "errorMessage" => $errorMessage];
+    }
+    
+    /**
+     * Add Validation check to form field from input attribute
+     *
+     * @param  string $inputName Name attribute of Input field
+     * @param  \Closure $condition Anonymous function with condition. 
+     *                             Must take 1 parameter (value of input) and return bool (true if no error, false otherwise)
+     * @param  string $errorMessage Error to be displayed if input fails validation
+     * @return void
+     */
+    private function validateAttribute(HTML5DOMElement $input, string $attribute, \Closure $condition, string $errorMessage) : bool {
         $inputName = $input->getAttribute("name");
+
+        // If there's already an error, no need for validate
+        if (isset($this->errors[$inputName]) === true) return false;
+        
         $formValue = $this->formValues->get($inputName);
         $attributeValue = $input->getAttribute($attribute);
-
-        if ($input->hasAttribute($attribute) && $condition($formValue, $attributeValue) === true) {
-            if (isset($this->errors[$inputName]) === false) {
-                $this->errors[$inputName] = sprintf($errorMessage, $attributeValue);
-            }
+        
+        if ($input->hasAttribute($attribute) && call_user_func($condition, $formValue, $attributeValue) === false) {
+            $this->errors[$inputName] = sprintf($errorMessage, $attributeValue);
             return false;
         }
         
+        return true;
+    }
+    
+    private function validateConstraints(string $inputName) : bool {
+        // If there's already an error, no need for validate
+        if (isset($this->errors[$inputName]) === true) return false;
+
+        // Don't change anything if no constraints defined
+        if (isset($this->userConstraints[$inputName]) === false) return true;
+
+        $formValue = $this->formValues->get($inputName);
+            
+        foreach ($this->userConstraints[$inputName] as $constraint) {
+            if (call_user_func($constraint["condition"], $formValue) === false) {
+                $this->errors[$inputName] = $constraint["errorMessage"];
+                return false;
+            }
+        }
+
         return true;
     }
 }
