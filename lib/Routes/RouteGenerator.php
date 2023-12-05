f70ba17a6047c5ecb887ce9760c200645028a499
Abderrahmane El Mahmi <elmahmi.a04@gmail.com>
2023-12-05 13:20:17 +0000

    Compiled Routes caching works locally & on vercel

--- lib/Routes/RouteGenerator.php
+++ lib/Routes/RouteGenerator.php
@@ -9,37 +9,44 @@ use Symfony\Component\Config;
 class RouteGenerator {
     private static $appDir = __DIR__."/../../app";
     private static $routes;
+    private static $cacheFolder = __DIR__."/../../cache";
+    private static $cacheFile = __DIR__."/../../cache/routes.cache";
 
     public static function generate(Request $request) : Routing\Matcher\CompiledUrlMatcher {
+        if (getenv("VERCEL_ENV") === false) {
+            self::buildRoutes();
+        }
+        
+        $context = new Routing\RequestContext();
+        $context->fromRequest($request);   
+
+        return new Routing\Matcher\CompiledUrlMatcher(self::getRoutes(), $context);
+    }
+
+    public static function buildRoutes() {
         $fileLocator = new Config\FileLocator(self::$appDir);
         $routeLoader = new Routing\Loader\YamlFileLoader($fileLocator);
         self::$routes = $routeLoader->load("routes.yaml");
 
         self::$routes->addCollection(StaticRoutes::generate());
 
-        $context = new Routing\RequestContext();
-        $context->fromRequest($request);
-
-        $compiledRoutes = self::compileRoutes();
-
-        return new Routing\Matcher\CompiledUrlMatcher($compiledRoutes, $context);
+        self::compileRoutes();
     }
 
-    private static function compileRoutes() : array {
-        $cacheFolder = __DIR__."/cache";
-        $cacheFile = $cacheFolder . "/routes.cache";
+    private static function getRoutes() : array {
+        return unserialize(file_get_contents(self::$cacheFile));
+    }
+    
+    private static function compileRoutes() {
+        if (file_exists(self::$cacheFolder) === false) {
+            mkdir(self::$cacheFolder, recursive: true);
+        }
 
-        // if (file_exists($cacheFolder) === false) {
-        //     mkdir($cacheFolder, recursive: true);
-        // }
+        if (file_exists(self::$cacheFile) && filemtime(self::$appDir . "/routes.yaml") < filemtime(self::$cacheFile)) {
+            return self::getRoutes();
+        }
         
-        // if (file_exists($cacheFile) && filemtime(self::$appDir . "/routes.yaml") < filemtime($cacheFile)) {
-        //     return unserialize(file_get_contents($cacheFile));
-        // }
-
         $compiledRoutes = (new Routing\Matcher\Dumper\CompiledUrlMatcherDumper(self::$routes))->getCompiledRoutes();
-        // file_put_contents($cacheFile, serialize($compiledRoutes));
-        
-        return $compiledRoutes;
+        file_put_contents(self::$cacheFile, serialize($compiledRoutes));
     }
 }
